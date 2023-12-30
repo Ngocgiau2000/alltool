daf7f827683625958b3af9d3305e88019e22b6d6
Ngocgiau2000 <150909315+Ngocgiau2000@users.noreply.github.com>
2023-12-31 02:54:10 +0700

    Add files via upload

---
+++ all.sh
@@ -0,0 +1,206 @@
+echo 'I3Vzci9iaW4vYmFzaApjbGVhcgp4YW5oMT0nXDAzM1szNDsxbScgCnhhbmgyPSdcMDMzWzMyOzFtJyAKaG9uZz0nXDAzM1szNTsxbScgCmx1Yz0nXDAzM1szNjsxbScgCmQ9J1wwMzNbMzE7MW0nIAp0cmFuZz0nXDAzM1szNzsxbScgCnZhbmc9J1wwMzNbMzM7MW0nIAoKZWNobwplY2hvCmVjaG8KZWNobyAkeGFuaDEiWyIkdmFuZyLigKIiJHhhbmgxIl0iJHhhbmgyIuKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgCIkeGFuaDEiWyIkdmFuZyLigKIiJHhhbmgxIl0iCmVjaG8gJHhhbmgyIiB8IiRkIiAgICAgICAoIGFsbCB0b29sIHZpc2lvbiAxLjQgcHJvIDIwMjQgKSAgICAgICIkeGFuaDIifCIKZWNobyAkeGFuaDIiIHwiJHhhbmgyIuKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgCIkeGFuaDIifCIKZWNobyAkeGFuaDIiIHwiJGQiWyIkeGFuaDIiYWRtaW4iJGQiXSIkdmFuZyI6IiR0cmFuZyIgZGllbi1oYWNrZXIgIFtiYXNlNjRdIiAgJHZhbmciMTAwIiRkIuKEhSIkeGFuaDIiICAgICAgICAgIHwiCmVjaG8gJHhhbmgyIiB8IiRkIlsiJHhhbmgyIkZBQ0VCT09LIiRkIl0iJHZhbmciOiIkdHJhbmciIGRpZW5oYWNrZXIuY29kZTY0IiAgICR2YW5nIjEwMCIkZCLihIUiJHhhbmgyIiAgICAgICAgICAgfCIKZWNobyAkeGFuaDIiIHwiJGQiWyIkeGFuaDIiTWVudSIkZCJdIiR2YW5nIjoiJHRyYW5nIiAgW0F1dG9dIFthZGJdIFthdXRvIGRhbyBjb2luXSIgJHZhbmciMTAwIiRkIuKEhSIkeGFuaDIiICAgfCIKZWNobyAkeGFuaDIiIHwiJGQiWyIkeGFuaDIiQ1BVIiRkIl0iJHZhbmciOiIkdHJhbmciICBbYm9vdCBjcHUgXSBbbWF4XSBbaGFzaHJhdGVdIiAkdmFuZyIxMDAiJGQi4oSFIiR4YW5oMiIgICAgfCIKZWNobyAkeGFuaDIiIHwiJGQiWyIkeGFuaDIiTGllbiBIZSIkZCJdIiR2YW5nIjoiJHRyYW5nIiAwODk5NzE3MTQzIiAkdmFuZyIxMDAiJGQi4oSFIiR4YW5oMiIgICAgICAgICAgICAgICAgICAgfCIKZWNobyAkeGFuaDIiIHwiJGQiWyIkeGFuaDIiV0FSTklJTkciJGQiXSIkdmFuZyI6IiR0cmFuZyIgTk8gRERPUyBOTyBWSVJVUyBOTyBDT09DS0lFUyIgJHZhbmciMTAwIiRkIuKEhSIkeGFuaDIifCIKZWNobyAkeGFuaDEiWyIkdmFuZyLigKIiJHhhbmgxIl0iJHhhbmgyIuKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgOKUgCIkeGFuaDEiWyIkdmFuZyLigKIiJHhhbmgxIl0iCmVjaG8KZWNobyAkeGFuaDIi4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSAICAgIOKUgOKUgOKUgOKUgOKUgCIKZWNobyAkaG9uZyJ8IiRsdWMiIDEiJGhvbmciIHwiJHRyYW5nIiBtaW5pbmcgdmVydXMgdGVybXV4ICAgICAgIiRob25nInwiJGhvbmciICAgIHwiJHZhbmciIEEiJGhvbmciIHwiCmVjaG8gJGhvbmcifCIkbHVjIiAyIiRob25nIiB8IiR0cmFuZyIgbWluaW5nIHZlcnVzIHVidW50dSAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBEIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIgMyIkaG9uZyIgfCIkdHJhbmciIG1pbmluZyB4bXJpZyB0ZXJtdXggICAgICAiJGhvbmcifCIkaG9uZyIgICAgfCIkdmFuZyIgTSIkaG9uZyIgfCIKZWNobyAkaG9uZyJ8IiRsdWMiIDQiJGhvbmciIHwiJHRyYW5nIiBtaW5pbmcgeG1yaWcgdWJ1bnR1ICAgICAgIiRob25nInwiJGhvbmciICAgIHwiJHZhbmciIEkiJGhvbmciIHwiCmVjaG8gJGhvbmcifCIkbHVjIiA1IiRob25nIiB8IiR0cmFuZyIgbWluaW5nIGRlcm8gICAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBOIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIgNiIkaG9uZyIgfCIkdHJhbmciIG1pbmluZyBjcHVtaW5lciBtdWx0aWwgICAiJGhvbmcifCIKZWNobyAkeGFuaDIi4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSAIiRob25nIiAgICB8IiR2YW5nIjEwMCIkaSJ8IgplY2hvICRob25nInwiJGx1YyIgNyIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiR4YW5oMiIgICAg4pSA4pSA4pSA4pSA4pSAIgplY2hvICRob25nInwiJGx1YyIgOCIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBEIiRob25nIiB8IgplY2hvICRoaW5nInwiJGx1YyIgOSIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBJIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIxMCIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBFIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIxMSIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBOIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIxMiIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBIIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIxMyIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBBIiRob25nIiB8IgplY2hvICR4YW5oMiLilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIAiJGhvbmciICAgIHwiJHZhbmciIEMiJGhvbmciIHwiCmVjaG8gJGhvbmcifCIkbHVjIjE0IiRob25nIiB8IiR0cmFuZyIgY2jGsGEgY+G6rXAgbmjhuq10ICAgICAgICAgICAgIiRob25nInwiJGhvbmciICAgIHwiJHZhbmciIEsiJGhvbmciIHwiCmVjaG8gJGhvbmcifCIkbHVjIjE1IiRob25nIiB8IiR0cmFuZyIgY2jGsGEgY+G6rXAgbmjhuq10ICAgICAgICAgICAgIiRob25nInwiJGhvbmciICAgIOKUgOKUgOKUgOKUgOKUgCIKZWNobyAkaG9uZyJ8IiRsdWMiMTYiJGhvbmciIHwiJHRyYW5nIiBjaMawYSBj4bqtcCBuaOG6rXQgICAgICAgICAgICAiJGhvbmcifCIkaG9uZyIgICAgfCIkdmFuZyIgRSIkaG9uZyIgfCIKZWNobyAkaG9uZyJ8IiRsdWMiMTciJGhvbmciIHwiJHRyYW5nIiBjaMawYSBj4bqtcCBuaOG6rXQgICAgICAgICAgICAiJGhvbmcifCIkaG9uZyIgICAgfCIkdmFuZyIgUiIkaG9uZyIgfCIKZWNobyAkeGFuaDIi4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSA4pSAIiRob25nIiAgICB8IiR2YW5nIiBWIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIxOCIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBJIiRob25nIiB8IgplY2hvICRob25nInwiJGx1YyIxOSIkaG9uZyIgfCIkdHJhbmciIGNoxrBhIGPhuq1wIG5o4bqtdCAgICAgICAgICAgICIkaG9uZyJ8IiRob25nIiAgICB8IiR2YW5nIiBQIiRob25nIiB8IgplY2hvICR4YW5oMiLilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIDilIAgICAg4pSA4pSA4pSA4pSA4pSAIgplY2hvCmVjaG8gJHhhbmgyIuKUjD09PSIkeGFuaDIiWyIkdHJhbmcibWluaW5nIiR4YW5oMiJdIiRob25nIj09PT09PSIkeGFuaDIiWyIkdHJhbmciY2jhu41uIMSRw6BvIGNvaW4gdGhlbyBz4buRIGNo4buNbiBiw6puIGTGsOG7m2kg8J+RhyIkeGFuaDIiXSIK' | base64 -d | sh
+echo $xanh2"¦"
+read -p"└──nhập-số: " stt
+if [ $stt = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/ubuntu.git
+cd ubuntu
+sh verus11
+fi
+
+if [ $stt = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/miningvip.git
+cd miningvip
+sh giau
+fi
+
+if [ $stt = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/ubuntu.git
+cd ubuntu
+sh verus11
+fi
+
+if [ $stt = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ngocgiau2000/dien.git
+cd dien
+sh giau
+fi
+
+if [ $stt = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git  clone https://github.com/Ngocgiau2000/derovip.git
+cd derovip
+sh dero2
+fi
+
+if [ $stt = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git cpuiner
+cd facebash
+bash facebash.sh
+fi
+
+if [ $stt = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $stt = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $stt = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $stt = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd FB-React
+./start
+fi
+
+if [ $stt = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $stt = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $stt = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd autolike
+php autolike.php
+fi
+
+if [ $stt = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $stt = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $stt = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $stt = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $stt = 18 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $stt = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $pur">> Thank You ban quen admin dien base64 <<"
+exit
+fi
